ad073bf340b66ace5bac5ed380c8ebbf0ed5937a
Iliyan Galabov <milenov556@gmail.com>
2023-12-27 11:59:35 +0200

    Added more setup files

--- MouseTracker/MouseTracker/settings.py
+++ MouseTracker/MouseTracker/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$fwcua$39s@o$al!&&z%(4z-*y7fw=go(1-oasf=o0=*ies$3z'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'MouseTracker.MouseTrackerApp'
 ]
 
 MIDDLEWARE = [
