8813defc9ecd0a7e6d52585e396a3cceaed7edbe
Iliyan Galabov <milenov556@gmail.com>
2024-01-03 17:18:58 +0200

    Added on_click function with functionality to print mouse positioning information on click

--- MouseTracker/MouseTracker/MouseTrackerApp/views.py
+++ MouseTracker/MouseTracker/MouseTrackerApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import pyautogui
+from pynput.mouse import Listener, Button
 
-# Create your views here.
+
+def on_click(x, y, button, pressed):
+    if button == Button.left and pressed:
+        current_x, current_y = pyautogui.position()
+        print(f"Left button clicked at ({x}, {y}). Current position: ({current_x}, {current_y})")
+
+
+with Listener(on_click=on_click) as listener:
+    listener.join()
